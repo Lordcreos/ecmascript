eb602abfd76401188ab8393f23bbd08c567312f6
Lordcreos <leonardosanchez4h@hotmail.com>
2021-03-09 22:17:56 -0500

    clase  2

--- es6/index.js
+++ es6/index.js
@@ -22,3 +22,32 @@ console.log(epicPhrase);
 
 let epicPrhase2 = `${hello} ${world}`;
 console.log(epicPrhase2);
+
+let lorem ="inserte frase 1 aqui \n" + "inserte frase 2 aqui";
+
+let lorem2 = `ahora si esto es una frase epica
+y est otra frase epica lol
+`;
+console.log(lorem);
+console.log(lorem2);
+
+let person = {
+    'Nombre' :  'leo',
+    'edad' : 24,
+    'country' : 'MX'
+}
+
+console.log(person.Nombre, person.edad, person.country);
+
+//en es6
+
+let {Nombre, edad , country} = person;
+
+console.log( edad , country, Nombre);
+
+//elemento de propagaciómn
+let team1= ['pepe','pedro','guzman'];
+let team2 =['yair','vacha','norberto'];
+
+let union = [ 'leo', ...team1,...team2 ];
+console.log(union);
