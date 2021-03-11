f03a9683009d1b98355d19179f140f7bdb28d074
Lordcreos <leonardosanchez4h@hotmail.com>
2021-03-10 23:55:40 -0500

    Ecmascript8

---
+++ es7/index.js
@@ -0,0 +1,14 @@
+let numbers =[1,2,3,4,7,8]
+
+if(numbers.includes(7)){
+    console.log('encontrmaos tu numero');
+}else{
+    console.log('no se encuentra');
+}
+
+let base = 4;
+let exponente = 3 ;
+
+let res = base ** exponenteM
+console.log(res);
+
